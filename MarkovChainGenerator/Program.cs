5788cbae247bc6c831a265e583a25e09aaaf1071
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2022-08-06 22:55:25 +0900

    Markov Chain init

---
+++ MarkovChainGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MarkovChainGenerator // Note: actual namespace depends on the project name.
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+}
