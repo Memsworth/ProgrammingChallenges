47efaa5c355f1a8e87b10fd682ac80440b90fceb
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2022-08-17 01:15:18 +0900

    Image2ASCII implementation done.  Implementation on the UI for the app pending.

---
+++ ImageTwoASCII/ConvertCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageTwoASCII;
+
+public class ConvertCommand : ICommand
+{
+    public bool Execute(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
--- ImageTwoASCII/LoadCommand.cs
+++ ImageTwoASCII/LoadCommand.cs
@@ -9,10 +9,8 @@ public class LoadCommand : ICommand
         var loadMatch = Regex.Match(input!, "loadfile (.*)");
         if (!loadMatch.Success) return false;
         var filePath = loadMatch.Groups[1].Value;
-        if (!File.Exists(filePath))
-        {
-            Console.WriteLine($"{new ArgumentException("File doesn't exist")}");
-        }
-        return true;
+        if (File.Exists(filePath)) return true;
+        Console.WriteLine($"{new ArgumentException("File doesn't exist")}");
+        return false;
     }
 }
